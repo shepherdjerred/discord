32cc60b6ed1818f3c8b3a14fdec258dd67a30c2b
Jerred Shepherd <github@sjer.red>
2023-09-28 16:33:37 -0700

    basic

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://glitter-boys.com",
+  integrations: [tailwind(), sitemap(), mdx()],
+});
