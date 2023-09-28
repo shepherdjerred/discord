cf81dc18f19a8f91e11965ff76eef71a127ecd4a
Jerred Shepherd <github@sjer.red>
2023-09-27 22:04:35 -0700

    init

--- packages/frontend/src/components/Page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./Container.js";
-
-export interface PageProps {
-  children: React.ReactNode;
-}
-
-export function Page(props: PageProps) {
-  return <Container>{props.children}</Container>;
-}
--- packages/frontend/src/components/Spinner.tsx
+++
@@ -1,6 +0,0 @@
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export function Spinner() {
-  return <FontAwesomeIcon icon={faSpinner} className="animate-spin" />;
-}
