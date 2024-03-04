b48e31c03bb62753f0dec9bc256fc561051f8fad
Jerred Shepherd <github@sjer.red>
2024-03-03 17:30:46 -0800

    explicit region

--- packages/backend/src/league/util.ts
+++ packages/backend/src/league/util.ts
@@ -1,8 +1,11 @@
+import * as Sentry from "https://deno.land/x/sentry/index.mjs";
+
 export function logErrors(fn: () => Promise<unknown>) {
   return async () => {
     try {
       await fn();
     } catch (e) {
+      Sentry.captureException(e);
       console.error(e);
     }
   };
